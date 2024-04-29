01f3dbd73a0725890819108e4e9a7a3029ddbefc
alojine <benaspaliokas@gmail.com>
2024-04-29 17:59:29 +0300

    initial commit

---
+++ src/main/java/com/ba/paymentprocessing/PaymentprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.ba.paymentprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentprocessingApplication.class, args);
+	}
+
+}
