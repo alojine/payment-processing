79178bcd65b2ddeef087480b7c9e9d9fe109debf
alojine <benaspaliokas@gmail.com>
2024-05-01 13:09:58 +0300

    Added details logging to Payment request IOUtils dependency

---
+++ src/main/java/com/ba/paymentprocessing/service/PaymentRequestFilter.java
@@ -0,0 +1,38 @@
+package com.ba.paymentprocessing.service;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class PaymentRequestFilter implements Filter {
+    private final Logger logger = LoggerFactory.getLogger(PaymentRequestFilter.class);
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+
+        String clientIp = httpRequest.getRemoteAddr();
+        String method = httpRequest.getMethod();
+        String requestURI = httpRequest.getRequestURI();
+
+        StringBuilder logMessage = new StringBuilder();
+        logMessage.append("Client IP: ").append(clientIp).append(", Method: ").append(method).append(", URI: ").append(requestURI);
+
+        if (method.equalsIgnoreCase("POST")) {
+            logMessage.append(", Request Body: ").append(IOUtils.toString(servletRequest.getInputStream()));
+        }
+
+        logger.info(logMessage.toString());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
